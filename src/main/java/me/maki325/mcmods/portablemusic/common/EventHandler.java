866aa9d19bce5ed21f251185b4dee71e90d0c849
Maki325 <markoni521@gmail.com>
2022-07-29 15:20:50 +0200

    Removed boombox capability

--- src/main/java/me/maki325/mcmods/portablemusic/common/EventHandler.java
+++ src/main/java/me/maki325/mcmods/portablemusic/common/EventHandler.java
@@ -1,28 +1,19 @@
 package me.maki325.mcmods.portablemusic.common;
 
 import me.maki325.mcmods.portablemusic.PortableMusic;
-import me.maki325.mcmods.portablemusic.common.capabilities.boombox.BoomboxProvider;
-import me.maki325.mcmods.portablemusic.common.capabilities.boombox.IBoomboxCapability;
 import me.maki325.mcmods.portablemusic.common.entities.SoundItemEntity;
 import me.maki325.mcmods.portablemusic.common.items.PMItems;
 import me.maki325.mcmods.portablemusic.common.savedata.SoundManagerSaveData;
 import me.maki325.mcmods.portablemusic.server.ServerSoundManager;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.List;
 
 @Mod.EventBusSubscriber(modid = PortableMusic.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class EventHandler {
@@ -57,47 +48,11 @@ public class EventHandler {
             EventHandler::loadSoundManagerSaveData,
             EventHandler::createSoundManagerSaveData,
             PortableMusic.MODID + "_sound_manager");
-
     }
 
     @SubscribeEvent public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
         if(!(event.getEntity() instanceof ServerPlayer)) return;
         ServerSoundManager.getInstance().sync(event.getEntity());
-    }
-
-    @SubscribeEvent public static void registerCaps(RegisterCapabilitiesEvent event) {
-        event.register(IBoomboxCapability.class);
-    }
-
-    @SubscribeEvent public void onEntityJoinWorld(EntityJoinLevelEvent event) {
-        // Some client setup code
-        if(!(event.getEntity() instanceof Player)) return;
-        if(event.getLevel().isClientSide) return;
-        ServerPlayer joinedPlayer = (ServerPlayer) event.getEntity();
-
-        List<Player> playerList = event.getLevel().getNearbyPlayers(
-            TargetingConditions.forNonCombat(), joinedPlayer, joinedPlayer.getBoundingBox().inflate(10));
-
-        for(Player player : playerList) {
-            for(ItemStack itemStack : player.getInventory().items) {
-                if(!itemStack.is(PMItems.BOOMBOX_ITEM.get())) continue;
-                LazyOptional<IBoomboxCapability> lazyOptional = itemStack.getCapability(BoomboxProvider.BOOMBOX_CAPABILITY);
-                IBoomboxCapability iBoomboxCapability = lazyOptional.orElse(null);
-                if(iBoomboxCapability == null) continue;
-            }
-        }
-
-        for(ItemStack itemStack : joinedPlayer.getInventory().items) {
-            if(!itemStack.is(PMItems.BOOMBOX_ITEM.get())) continue;
-            LazyOptional<IBoomboxCapability> lazyOptional = itemStack.getCapability(BoomboxProvider.BOOMBOX_CAPABILITY);
-            IBoomboxCapability iBoomboxCapability = lazyOptional.orElse(null);
-            if(iBoomboxCapability == null) continue;
-        }
-
-        for(ItemStack itemStack : joinedPlayer.getInventory().items) {
-            if(!itemStack.is(PMItems.BOOMBOX_ITEM.get())) continue;
-            itemStack.getOrCreateTag();
-        }
     }
 
 }
